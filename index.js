6d2e890b3bf0e85dbaab9e19513885f2cc3e714d
briwa <briwa@piktochart.com>
2016-07-16 18:35:24 +0800

    Remove Coffeescript because why not

---
+++ index.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(robot, scripts) {
+  var scriptsPath = path.resolve(__dirname, 'src');
+  
+  fs.exists(scriptsPath, function(exists) {
+    if (exists) {
+      var ref = fs.readdirSync(scriptsPath);
+      for (var i = 0; i < ref.length; i++) {
+        var script = ref[i];
+        robot.loadFile(scriptsPath, script);
+      }
+    }
+  });
+};
